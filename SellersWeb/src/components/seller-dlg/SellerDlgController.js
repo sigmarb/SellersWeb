11331d4bf0cd61ff8a2d8f5f6d2c321357697f22
egillgautur <egills14@ru.is>
2016-03-09 18:50:06 +0000

    sellers-dlg

--- SellersWeb/src/components/seller-dlg/SellerDlgController.js
+++ SellersWeb/src/components/seller-dlg/SellerDlgController.js
@@ -6,14 +6,29 @@ function SellersDlgController($scope) {
 	$scope.seller = {
 		name: "",
 		category: "",
+        imagePath: ""
 
 	};
-
+    
+    $scope.setSellers = function setSellers(Name, Category, Image) {
+        $scope.seller.name = Name;
+        $scope.seller.category = Category;
+        $scope.seller.imagePath = Image;
+    }
+    
 	$scope.onOk = function onOk(){
 		//TODO: VALIDATION
 		if ($scope.seller.name.length === 0) {
-			//birta validation skilaboð!
+			$scope.errorMessage = "Invalid name!";
 		}
+        
+        if ($scope.seller.category.length === 0) {
+            $scope.errorMessage = "Invalid category!";
+        }
+        
+        if ($scope.seller.imagePath.length === 0) {
+            $scope.errorMessage = "Invalid image url!";
+        }
 		$scope.$close($scope.seller);
 	};
 
