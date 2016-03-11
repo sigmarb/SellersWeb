af45a0d83402179aab7495a6609971c8c78cde2e
Steinar Marinó Hilmarsson <SteinarMarino@clients-pool-1.nat.ru.is>
2016-03-11 17:51:17 +0000

    tungumal check

--- SellersWeb/.build/scripts/app-8e58c3ae.js
+++ SellersWeb/.build/scripts/app-8e58c3ae.js
@@ -137,6 +137,94 @@ function ProductDlgController($scope, centrisNotify) {
 }); 
 "use strict";
 
+angular.module("project3App").controller("sellersDetailsController", ["$scope", "AppResource", "$routeParams", "ProductDlg",
+function sellersDetailsController($scope, AppResource, $routeParams, ProductDlg) {
+    $scope.id = $routeParams.id;
+	$scope.seller = {};
+	$scope.sellerProduct = [];
+	$scope.sellerTop10Product = [];
+    $scope.DisplayAdd = true;
+    $scope.isLoading = true;
+    $scope.selectedProduct = {
+        name: "",
+        price: "",
+        quantitySold: "",
+        quantityInStock: "",
+        imagePath: ""
+    };
+	$scope.sellerTop10Product = [];
+	var sellerID = parseInt($scope.id);
+    
+    function getSelectedProduct() {
+        return $scope.selectedProduct;
+    }
+
+	AppResource.getSellerDetails(sellerID).success(function(seller) {
+		$scope.seller = seller;
+        $scope.isLoading = false;
+	}).error(function(){
+        $scope.isLoading = false;
+    });
+
+	AppResource.getSellerProducts(sellerID).success(function(sellerProduct)
+	{
+		$scope.sellerProduct = sellerProduct;
+		sellerProduct.sort(compare);
+		$scope.sellerTop10Product = sellerProduct.slice(0, 10);
+        $scope.isLoading = false;
+	}).error(function(){
+        $scope.isLoading = false;
+    });
+
+    	function compare(a,b) 
+	{
+  		if (a.quantitySold > b.quantitySold) 
+  		{
+    		return -1;
+  		}
+  		else if (a.quantitySold  < b.quantitySold) 
+  		{
+    		return 1;
+  		}
+  		else 
+  		{
+    		return 0;
+  		}
+	}
+    
+    $scope.onAddProduct = function onAddProduct() {
+        console.log("HERNA!");
+        $scope.DisplayAdd = false;
+        ProductDlg.show().then(function(product){
+            console.log("BLA!");
+            console.log(product);
+            AppResource.addSellerProduct(sellerID, product).success(function(product){
+                AppResource.getSellerProducts(sellerID).success(function(sellerProduct)
+	{
+		$scope.sellerProduct = sellerProduct;
+		sellerProduct.sort(compare);
+		$scope.sellerTop10Product = sellerProduct.slice(0, 10);
+        $scope.isLoading = false;
+	}).error(function(){
+        $scope.isLoading = false;
+    });
+
+                
+                
+                console.log("HERNAKKKK!");
+                $scope.DisplayAdd = true;
+                $scope.isLoading = false;
+            }).error(function(){
+                console.log("ERROIR");
+                //TODO
+            });
+        });
+    };
+    
+}]);
+	//$scope.sellerTop10Product = $scope.sellerProduct.slice(0,10);
+"use strict";
+
 /**
  * This module serves as the main resource object for our app, i.e.
  * the object which connects to our REST backend and loads/saves data.
@@ -320,94 +408,6 @@ function AppResource() {
 });
 "use strict";
 
-angular.module("project3App").controller("sellersDetailsController", ["$scope", "AppResource", "$routeParams", "ProductDlg",
-function sellersDetailsController($scope, AppResource, $routeParams, ProductDlg) {
-    $scope.id = $routeParams.id;
-	$scope.seller = {};
-	$scope.sellerProduct = [];
-	$scope.sellerTop10Product = [];
-    $scope.DisplayAdd = true;
-    $scope.isLoading = true;
-    $scope.selectedProduct = {
-        name: "",
-        price: "",
-        quantitySold: "",
-        quantityInStock: "",
-        imagePath: ""
-    };
-	$scope.sellerTop10Product = [];
-	var sellerID = parseInt($scope.id);
-    
-    function getSelectedProduct() {
-        return $scope.selectedProduct;
-    }
-
-	AppResource.getSellerDetails(sellerID).success(function(seller) {
-		$scope.seller = seller;
-        $scope.isLoading = false;
-	}).error(function(){
-        $scope.isLoading = false;
-    });
-
-	AppResource.getSellerProducts(sellerID).success(function(sellerProduct)
-	{
-		$scope.sellerProduct = sellerProduct;
-		sellerProduct.sort(compare);
-		$scope.sellerTop10Product = sellerProduct.slice(0, 10);
-        $scope.isLoading = false;
-	}).error(function(){
-        $scope.isLoading = false;
-    });
-
-    	function compare(a,b) 
-	{
-  		if (a.quantitySold > b.quantitySold) 
-  		{
-    		return -1;
-  		}
-  		else if (a.quantitySold  < b.quantitySold) 
-  		{
-    		return 1;
-  		}
-  		else 
-  		{
-    		return 0;
-  		}
-	}
-    
-    $scope.onAddProduct = function onAddProduct() {
-        console.log("HERNA!");
-        $scope.DisplayAdd = false;
-        ProductDlg.show().then(function(product){
-            console.log("BLA!");
-            console.log(product);
-            AppResource.addSellerProduct(sellerID, product).success(function(product){
-                AppResource.getSellerProducts(sellerID).success(function(sellerProduct)
-	{
-		$scope.sellerProduct = sellerProduct;
-		sellerProduct.sort(compare);
-		$scope.sellerTop10Product = sellerProduct.slice(0, 10);
-        $scope.isLoading = false;
-	}).error(function(){
-        $scope.isLoading = false;
-    });
-
-                
-                
-                console.log("HERNAKKKK!");
-                $scope.DisplayAdd = true;
-                $scope.isLoading = false;
-            }).error(function(){
-                console.log("ERROIR");
-                //TODO
-            });
-        });
-    };
-    
-}]);
-	//$scope.sellerTop10Product = $scope.sellerProduct.slice(0,10);
-"use strict";
-
 angular.module("project3App").controller("SellerDlgController",
 function SellerDlgController($scope, centrisNotify, $translate) {
 
@@ -530,147 +530,6 @@ function SellersController($scope, AppResource, centrisNotify, SellerDlg, $trans
 			$translate.use(key);
 		};
 });
-"use strict";
-
-/**
- * @ngdoc object
- * @name general.tableSort
- * @description
- * Use this directive to sort columns in a table. To use it you add
- * the directive to the table row <tr>, then you have to add data-columnName
- * to each <th> which should be sortable. The directive can take a string
- * parameter which will be used as the default column to be sorted.
- *
- * Note that this directive is currently a bit brutal, i.e. it will remove
- * ALL css classes from the table headings when a column is no longer the default
- * sort column (this may have to change later).
- *
- * Dependencies:
- *
- * * Depends on Font Awesome for the caret icon.
- *
- * @example
- * ```html
- * <!-- a) For each column that should be sortable, add the data-columnName attribute,
- * with the value of the sort expression: -->
- *
- * <tr table-sort="Name">
- *     <th data-columnName="Name">Name</th>
- *     <!-- etc. for each column in the table -->;
- * </tr>
- *
- * <!-- b) in your ng-repeat for the table rows, ensure you are using orderBy filter
- * with predicate and reverse (which are scope variables created by this
- * directive): -->
- *
- * <tr ng-repeat="stuff in stuffz | orderBy:predicate:reverse">
- *
- * <!-- If the sort order should be reversed initially, place a minus sign in front of
- *      the name of the default column: -->
- * <tr table-sort="-Name">
- * ```
- */
-angular.module("sharedServices").directive("tableSort", function () {
-	function link(scope, element, attrs) {
-		var tableChildren = element.children();
-		var defaultColumn = attrs.tableSort;
-		var reverse = attrs["reverse"] || "reverse";
-		var predicate = attrs["predicate"] || "predicate";
-
-		if (defaultColumn.length > 0 && defaultColumn[0] === "-") {
-			scope[reverse] = true;
-			scope[predicate] = defaultColumn.substring(1);
-		} else {
-			scope[reverse] = false;
-			scope[predicate] = defaultColumn;
-		}
-
-		function initClickHandlers() {
-			angular.forEach(tableChildren, function(value) {
-				var el = angular.element(value);
-				var columnName = el.attr("columnName") || el.attr("data-columnName");
-
-				// Only attach a click handler if:
-				// a) the <th> element has a non-empty data-columnName attribute.
-				// b) there isn't already a click handler there
-				if (isSortColumn(columnName)) {
-					if (el.attr("ng-click") === undefined) {
-						el.bind("click", function(e) {
-							scope.$apply(function() {
-								sortNotes(columnName);
-							});
-						});
-					}
-				}
-			});
-		}
-		initClickHandlers();
-
-		// In case the number of columns is variable (or generated using
-		// a ng-repeat), we need to watch for new columns.
-		// TODO: doesn't quite work yet!
-		/*
-		scope.$watch(element.children().length, function(){
-			console.log("Length of elements has changed!");
-			initClickHandlers();
-		});
-		*/
-		// Setup the columns adding <i> to all table headers and
-		// adds caret class to the default column.
-		function setup() {
-			angular.forEach(tableChildren, function(value) {
-				var el = angular.element(value);
-				var columnName = el.attr("columnName") || el.attr("data-columnName");
-				if (isSortColumn(columnName)) {
-					el.prepend("<i></i>");
-					if (columnName === defaultColumn || columnName === "'" + defaultColumn + "'") {
-						el.children().addClass("fa fa-caret-" + (scope[reverse] ? "up" : "down"));
-					}
-				}
-			});
-		}
-
-		function isSortColumn(columnName) {
-			return columnName !== undefined && columnName.length > 0;
-		}
-
-		function sortNotes(sortBy) {
-			if (scope[predicate] === sortBy) {
-				scope[reverse] = !scope[reverse];
-			}
-			scope[predicate] = sortBy;
-			angular.forEach(tableChildren, function(value) {
-				var el = angular.element(value);
-				var columnName = el.attr("columnName") || el.attr("data-columnName");
-				if (isSortColumn(columnName)) {
-					el.children().removeClass();
-					if (scope[predicate] === columnName) {
-						el.children().addClass("fa fa-caret-" + (sortDown(sortBy) ? "up" : "down"));
-					}
-				}
-			});
-		}
-
-		// Helper function:
-		function sortDown(sortType) {
-			return scope[reverse] && scope[predicate] === sortType;
-		}
-
-		scope.$watch("tableSort", function(value) {
-			if (value) {
-				defaultColumn = value;
-			}
-			setup();
-		});
-	}
-
-	return {
-		restrict: "A",
-		replace: false,
-		link: link
-	};
-});
-
 "use strict";
 
 /**
@@ -810,5 +669,145 @@ function(toastr, toastrConfig, $translate, $rootScope) {
 		successWithUndo: function successWithUndo(messageKey, undoID) {
 			notificationFunctionWithUndo(messageKey, undoID);
 		}
+	};
+});
+"use strict";
+
+/**
+ * @ngdoc object
+ * @name general.tableSort
+ * @description
+ * Use this directive to sort columns in a table. To use it you add
+ * the directive to the table row <tr>, then you have to add data-columnName
+ * to each <th> which should be sortable. The directive can take a string
+ * parameter which will be used as the default column to be sorted.
+ *
+ * Note that this directive is currently a bit brutal, i.e. it will remove
+ * ALL css classes from the table headings when a column is no longer the default
+ * sort column (this may have to change later).
+ *
+ * Dependencies:
+ *
+ * * Depends on Font Awesome for the caret icon.
+ *
+ * @example
+ * ```html
+ * <!-- a) For each column that should be sortable, add the data-columnName attribute,
+ * with the value of the sort expression: -->
+ *
+ * <tr table-sort="Name">
+ *     <th data-columnName="Name">Name</th>
+ *     <!-- etc. for each column in the table -->;
+ * </tr>
+ *
+ * <!-- b) in your ng-repeat for the table rows, ensure you are using orderBy filter
+ * with predicate and reverse (which are scope variables created by this
+ * directive): -->
+ *
+ * <tr ng-repeat="stuff in stuffz | orderBy:predicate:reverse">
+ *
+ * <!-- If the sort order should be reversed initially, place a minus sign in front of
+ *      the name of the default column: -->
+ * <tr table-sort="-Name">
+ * ```
+ */
+angular.module("sharedServices").directive("tableSort", function () {
+	function link(scope, element, attrs) {
+		var tableChildren = element.children();
+		var defaultColumn = attrs.tableSort;
+		var reverse = attrs["reverse"] || "reverse";
+		var predicate = attrs["predicate"] || "predicate";
+
+		if (defaultColumn.length > 0 && defaultColumn[0] === "-") {
+			scope[reverse] = true;
+			scope[predicate] = defaultColumn.substring(1);
+		} else {
+			scope[reverse] = false;
+			scope[predicate] = defaultColumn;
+		}
+
+		function initClickHandlers() {
+			angular.forEach(tableChildren, function(value) {
+				var el = angular.element(value);
+				var columnName = el.attr("columnName") || el.attr("data-columnName");
+
+				// Only attach a click handler if:
+				// a) the <th> element has a non-empty data-columnName attribute.
+				// b) there isn't already a click handler there
+				if (isSortColumn(columnName)) {
+					if (el.attr("ng-click") === undefined) {
+						el.bind("click", function(e) {
+							scope.$apply(function() {
+								sortNotes(columnName);
+							});
+						});
+					}
+				}
+			});
+		}
+		initClickHandlers();
+
+		// In case the number of columns is variable (or generated using
+		// a ng-repeat), we need to watch for new columns.
+		// TODO: doesn't quite work yet!
+		/*
+		scope.$watch(element.children().length, function(){
+			console.log("Length of elements has changed!");
+			initClickHandlers();
+		});
+		*/
+		// Setup the columns adding <i> to all table headers and
+		// adds caret class to the default column.
+		function setup() {
+			angular.forEach(tableChildren, function(value) {
+				var el = angular.element(value);
+				var columnName = el.attr("columnName") || el.attr("data-columnName");
+				if (isSortColumn(columnName)) {
+					el.prepend("<i></i>");
+					if (columnName === defaultColumn || columnName === "'" + defaultColumn + "'") {
+						el.children().addClass("fa fa-caret-" + (scope[reverse] ? "up" : "down"));
+					}
+				}
+			});
+		}
+
+		function isSortColumn(columnName) {
+			return columnName !== undefined && columnName.length > 0;
+		}
+
+		function sortNotes(sortBy) {
+			if (scope[predicate] === sortBy) {
+				scope[reverse] = !scope[reverse];
+			}
+			scope[predicate] = sortBy;
+			angular.forEach(tableChildren, function(value) {
+				var el = angular.element(value);
+				var columnName = el.attr("columnName") || el.attr("data-columnName");
+				if (isSortColumn(columnName)) {
+					el.children().removeClass();
+					if (scope[predicate] === columnName) {
+						el.children().addClass("fa fa-caret-" + (sortDown(sortBy) ? "up" : "down"));
+					}
+				}
+			});
+		}
+
+		// Helper function:
+		function sortDown(sortType) {
+			return scope[reverse] && scope[predicate] === sortType;
+		}
+
+		scope.$watch("tableSort", function(value) {
+			if (value) {
+				defaultColumn = value;
+			}
+			setup();
+		});
+	}
+
+	return {
+		restrict: "A",
+		replace: false,
+		link: link
 	};
 });
