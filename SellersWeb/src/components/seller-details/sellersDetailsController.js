41400e5b1f6d63778977ff82ed75faf2f5eec400
sigmarb@gmail.com <sigmarb@Sigmars-MacBook-Pro.local>
2016-03-11 16:07:59 +0000

    sort products

--- SellersWeb/src/components/seller-details/sellersDetailsController.js
+++ SellersWeb/src/components/seller-details/sellersDetailsController.js
@@ -5,6 +5,7 @@ function sellersDetailsController($scope, AppResource, $routeParams) {
     $scope.id = $routeParams.id;
 	$scope.seller = {};
 	$scope.sellerProduct = [];
+	$scope.sellerTop10Product = [];
 	var sellerID = parseInt($scope.id);
 
 	AppResource.getSellerDetails(sellerID).success(function(seller) {
@@ -14,16 +15,26 @@ function sellersDetailsController($scope, AppResource, $routeParams) {
 	AppResource.getSellerProducts(sellerID).success(function(sellerProduct)
 	{
 		$scope.sellerProduct = sellerProduct;
+		sellerProduct.sort(compare);
+		$scope.sellerTop10Product = sellerProduct.slice(0, 10);
 	});
 
-	$scope.sellerTop10Product = $scope.sellerProduct.slice(0,10);
+	//$scope.sellerTop10Product = $scope.sellerProduct.slice(0,10);
 
-	var test = function test(){
-		console.log("ASDF");
-	};
+	function compare(a,b) 
+	{
+  		if (a.quantitySold > b.quantitySold) 
+  		{
+    		return -1;
+  		}
+  		else if (a.quantitySold  < b.quantitySold) 
+  		{
+    		return 1;
+  		}
+  		else 
+  		{
+    		return 0;
+  		}
+	}
 }
-
-
-
-
 ]);
